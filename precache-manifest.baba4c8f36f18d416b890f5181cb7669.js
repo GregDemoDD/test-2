286d4b2dfe9a6afb378abf8edb8f2ef7fcadd09d
Gregory Tan <greg@Gregorys-MacBook-Pro.local>
2020-03-16 02:51:05 +0800

    Updates

---
+++ precache-manifest.baba4c8f36f18d416b890f5181cb7669.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f53f076fba15adf48673d5a1ee483c2",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "19c901417a3dd604c1d0",
+    "url": "/test/static/css/main.4b8917c3.chunk.css"
+  },
+  {
+    "revision": "34f5470a048a810751bf",
+    "url": "/test/static/js/2.8364879f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/test/static/js/2.8364879f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c901417a3dd604c1d0",
+    "url": "/test/static/js/main.c0501be2.chunk.js"
+  },
+  {
+    "revision": "6bbc3f51202c28fa0a7b",
+    "url": "/test/static/js/runtime-main.5b4329d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
